c104fac42b9bdaecd3fc9174bddf2e592c0193fa
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-16 15:48:35 +0000

    Added initial check

--- src/main/java/info/charlieward/lousynetlockdownplugin/LousyNetLockdownPlugin.java
+++ src/main/java/info/charlieward/lousynetlockdownplugin/LousyNetLockdownPlugin.java
@@ -1,17 +1,34 @@
 package info.charlieward.lousynetlockdownplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import redis.clients.jedis.Jedis;
+
+import java.util.Objects;
 
 public final class LousyNetLockdownPlugin extends JavaPlugin {
 
+    public Jedis jedis = new Jedis();
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("LousyNet-LockdownPlugin v." + this.getDescription().getVersion() + " has loaded.");
+        String loadState = jedis.get("lockdownStatus");
+        if (loadState != "true" || loadState != "false") {
+            getLogger().info("[LousyNet-LockdownPlugin] Lockdown variable not set. Setting to false");
+            jedis.set("lockdownState", "false");
+        } else{
+            if (Objects.equals(loadState, "true")) {
+                getLogger().info("[LousyNet-LockdownPlugin] Lockdown is currently active server wide");
+            } else {
+                getLogger().info("[LousyNet-LockdownPlugin] Lockdown is not currently active server wide");
+            }
+        }
+
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("LousyNet-LockdownPlugin v." + this.getDescription().getVersion() + " has been disabled.");
     }
 }
