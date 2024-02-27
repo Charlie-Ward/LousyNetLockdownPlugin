d2736dae267e9b3b2452348c0d66523894d5f926
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-27 11:57:15 +0000

    Added the start logic

--- src/main/java/info/charlieward/lousynetlockdownplugin/LousyNetLockdownPlugin.java
+++ src/main/java/info/charlieward/lousynetlockdownplugin/LousyNetLockdownPlugin.java
@@ -12,19 +12,15 @@ public final class LousyNetLockdownPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("LousyNet-LockdownPlugin v." + this.getDescription().getVersion() + " has loaded.");
-        String loadState = jedis.get("lockdownStatus");
-        if (loadState != "true" || loadState != "false") {
-            getLogger().info("[LousyNet-LockdownPlugin] Lockdown variable not set. Setting to false");
+        String loadState = jedis.get("lockdownState");
+        if ( (loadState == null || loadState.isEmpty()) || (!loadState.equals("true") && !loadState.equals("false")) ) {
+            getLogger().info("[LousyNet-LockdownPlugin] Lockdown variable not set or set to invalid value. Setting to false");
             jedis.set("lockdownState", "false");
-        } else{
-            if (Objects.equals(loadState, "true")) {
-                getLogger().info("[LousyNet-LockdownPlugin] Lockdown is currently active server wide");
-            } else {
-                getLogger().info("[LousyNet-LockdownPlugin] Lockdown is not currently active server wide");
-            }
+        } else if (loadState.equals("true")) {
+            getLogger().info("[LousyNet-LockdownPlugin] A lockdown is currently active on the network.");
+        } else if (loadState.equals("false")) {
+            getLogger().info("[LousyNet-LockdownPlugin] No lockdown is currently active on the network");
         }
-
-
     }
 
     @Override
